41e0b57a06f6e599864f4ccee627da5fc436ecc6
Gerard Vivancos <gerard.vivancos@gmail.com>
2015-07-01 22:34:30 +0200

    Initial version

---
+++ pureftpd/pure-userinit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -f purepw.txt ]; then
+  echo "No purepw.txt file found. Will not init Pureftpd users"
+  exit 0
+fi
+
+while read  l; do
+  user=$(echo $l | cut -d":" -f1)
+  pw=$(echo $l | cut -d":" -f2 )
+  echo -e "$pw\n$pw"| pure-pw useradd $user -u ftpuser -d /dev/null/
+done < purepw.txt
